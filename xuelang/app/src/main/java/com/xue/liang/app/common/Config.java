2a4eb6b818e19064384a121df0da5fecb6336d67
jikun <jikun>
2016-10-14 12:01:29 +0800

    修改了默认参数请求不再添加电话号码

--- xuelang/app/src/main/java/com/xue/liang/app/common/Config.java
+++ xuelang/app/src/main/java/com/xue/liang/app/common/Config.java
@@ -5,7 +5,7 @@ package com.xue.liang.app.common;
  */
 public class Config {
 
-    public static String TEST_PHONE_NUMBER = "18000000000";//18000000000
+    public static String TEST_PHONE_NUMBER = "";//18000000000
     public static String TEST_MAC = "38:BC:1A:C5:DA:4F";//38:BC:1A:C5:DA:4F
     public static String TEST_TYPE = "2";//
 
--- xuelang/app/src/main/java/com/xue/liang/app/main/MainActivity.java
+++ xuelang/app/src/main/java/com/xue/liang/app/main/MainActivity.java
@@ -296,11 +296,11 @@ public class MainActivity extends FragmentActivity implements MainContract.View<
                         if (choiceListener.getWhich() == 0 && choiceListener.isChecked()) {
 
                             if (DeviceUtil.isPhone(getApplicationContext())) {
-                                 checkCallPermissions();//因为API 23（Android 6.0）需要检测电话权限所以。
+                                checkCallPermissions();//因为API 23（Android 6.0）需要检测电话权限所以。
                                // mainPresenter.sendCall(mphoneNum);
                             } else {
                                 checkCallPermissions();
-                               // mainPresenter.sendCall(mphoneNum);
+                              // mainPresenter.sendCall(mphoneNum);
                             }
 
 
